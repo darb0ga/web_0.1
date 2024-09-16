1d5a117f9d7e1753fa7c01562b057d4c3910a411
dasha <darya2005@ro.ru>
2024-09-16 22:47:42 +0300

    validation values

--- src/main/java/org/web/index.js
+++ src/main/java/org/web/index.js
@@ -1,44 +1,50 @@
 const results = document.getElementById("results");
-const data = new FormData(this);
 
-function validate(data) {
-    if (!data.x){
+const date = new Date();
+
+function validate(x, y, r) {
+    if (!x) {
         return "x is not defined";
     }
 
-    if (isNaN(data.x)){
+    if (isNaN(x)) {
         return "x must be a number";
     }
 
-    if(parseInt(data.x) > 3 || parseInt(data.x) < -5){
+    if (parseInt(x) > 3 || parseInt(x) < -5) {
         return "x must be in [-5; 3]";
     }
 
-    if(!data.r){
-        return "choose r, please";
+    if (r.length < 1) {
+        return "r is not defined";
     }
-
+    if (!y) {
+        return "y is not defined";
+    }
+    if (r.length > 1) {
+        return "choose one value of r";
+    }
     return null;
 }
 
-function check(data) {
-    var x = parseInt(data.x);
-    var y = parseInt(data.y);
-    var r = parseInt(data.r);
-
-    if(x>=0 && x<=r){
-        if(y<=0&&y>=(r/2)){
-            return true;
-        }
-        if(y>0){
-
-        }
+async function onSubmit() {
+    const startedTime = date.getTime();
+    var x = document.querySelector(".x");
+    var y = document.querySelector(".y")
+    var checkboxes = document.querySelectorAll('input[class="r"]:checked');
+    var r_values = [];
+    checkboxes.forEach((el) => {
+        r_values.push(el.name);
+    })
+    var errorText = validate(x.value, y.value, r_values);
+    if(!errorText){
+         //все хорошо продолжаем работу
+        // время ответа?
+        // заполнить форму с резами
+    }else{
+        //неверные данные
+        //просьба передеоать данные
     }
 
-
-}
-
-function getValue(){
-    var x = document.getElementById("x");
-
+    return [x.value, y.value, r_values];
 }
