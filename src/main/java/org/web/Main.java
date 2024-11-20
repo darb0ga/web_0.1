242ef3138a979f6f2562d057f8bb1b9f4cb9825c
Darya^_^ <77808924+darb0ga@users.noreply.github.com>
2024-11-20 16:48:44 +0300

    Update Main

--- src/main/java/org/web/Main.java
+++ src/main/java/org/web/Main.java
@@ -2,76 +2,93 @@ package org.web;
 
 import com.fastcgi.FCGIInterface;
 
+import java.time.LocalTime;
+import java.util.LinkedHashMap;
 import java.util.Properties;
 
 
 public class Main {
-    private static String resTable = """
-            {
-                "time": "%s",
-                "result": %b
-            }
-            """;
-
-    private static final String httpResponse = """
+    private static final String HTTP_RESPONSE = """
             Content-Type: application/json
                     
-            {"code":"%d","result":"%s","x":"%d","y":"%.3f","r":"%.1f","time":"%s","result":"%.3f"}
+            {"code":"%d","result":"%s","x":"%d","y":"%.3f","r":"%.1f","time":"%s","scriptTime":"%.3f"}
+            """;
+    private static final String HTTP_ERROR = """
+            Content-Type: application/json
+                    
+            {"code":"%d","result":"%s","time":"%s","scriptTime":"%.3f"}
             """;
 
     public static void main(String[] args) {
 
+        FCGIInterface fcgi = new FCGIInterface();
 
-        FCGIInterface fcgiInterface = new FCGIInterface();
-        while (fcgiInterface.FCGIaccept() >= 0) {
-
+        while (fcgi.FCGIaccept() >= 0) {
+            if (FCGIInterface.request.params.getProperty("REQUEST_METHOD").equals("GET")) {
                 long time = System.nanoTime();
                 Properties prop = System.getProperties();
                 String QUERY_STRING = prop.getProperty("QUERY_STRING");
                 if (!QUERY_STRING.isBlank()) {
 
-                    String[] responseData = QUERY_STRING.split("&");
-                    float x = Float.parseFloat(responseData[0].substring(2));
-                    int y = Integer.parseInt(responseData[1].substring(2));
-                    int r = Integer.parseInt(responseData[2].substring(2));
+                    try {
+                        LinkedHashMap<String, Float> params = parse(QUERY_STRING);
+                        boolean isValidated = !(params.isEmpty());
+                        boolean isHit = checkDot(params);
 
-
-                    //String getParams = System.getProperties().getProperty("QUERY_STRING");
-
-
-                    //System.out.println(FCGIInterface.request.params.getProperty("REQUEST_METHOD"));
-
-                    long startTime = System.currentTimeMillis();
-
-                    var checking = checkDot(x, y, r);
-                    long prossTime = System.currentTimeMillis() - startTime;
-
-
-                    var json = String.format(resTable, prossTime, checking);
-                    var response = String.format(httpResponse, json.getBytes().length + 2, json);
-
-                    System.out.println(response);
+                        if (isValidated) {
+                            System.out.printf((HTTP_RESPONSE) + "%n", 200, isHit, params.get("x").intValue(), params.get("y"), params.get("r"),
+                                    String.valueOf(LocalTime.now()).split("\\.")[0],
+                                    (double) (System.nanoTime() - time) / 1000000); //
+                        } else {
+                            System.out.printf((HTTP_ERROR) + "%n", 400, "Invalid values", String.valueOf(LocalTime.now()).split("\\.")[0],
+                                    (double) (System.nanoTime() - time) / 1000000);
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.printf((HTTP_ERROR) + "%n", 400, "Invalid values", String.valueOf(LocalTime.now()).split("\\.")[0],
+                                (double) (System.nanoTime() - time) / 1000000);
+                    } catch (Exception e) {
+                        System.out.printf((HTTP_ERROR) + "%n", 400, e.getMessage(), String.valueOf(LocalTime.now()).split("\\.")[0],
+                                (double) (System.nanoTime() - time) / 1000000);
+                    }
+                } else {
+                    System.out.printf((HTTP_ERROR) + "%n", 400, "Fill values", String.valueOf(LocalTime.now()).split("\\.")[0],
+                            (double) (System.nanoTime() - time) / 10000000);
                 }
-
+            }
         }
     }
 
-    private static boolean checkDot(float x, int y, int r) {
+    public static LinkedHashMap<String, Float> parse(String queryString) throws IndexOutOfBoundsException {
+        LinkedHashMap<String, Float> parameters = new LinkedHashMap<>();
+        String[] reses = queryString.split("&");
+        float x = Float.parseFloat(reses[0].substring(2));
+        float y = Float.parseFloat(reses[1].substring(2));
+        float r = Float.parseFloat(reses[2].substring(2));
+        parameters.put("x", x);
+        parameters.put("y", y);
+        parameters.put("r", r);
+
+
+        return parameters;
+    }
+
+
+    private static boolean checkDot(LinkedHashMap<String, Float> params) {
+        float x = params.get("x");
+        float y = params.get("y");
+        float r = params.get("r");
+
         if (x > 0) {
             if (y <= 0) {
-                return x <= r & y >= -r / 2;
+                return (x <= r) && (y >= (-r / 2));
             } else {
                 return x * x + y * y <= r * r;
             }
         } else {
             if (y <= 0) {
-                return x - ((float) r / 2) <= y;
+                return -x - (r / 2) <= y;
             }
             return false;
         }
     }
-
-    //отсылка ответа на страницу
-    //адо еще сделать так что бы при обновлении данные не пропадали
-
 }
